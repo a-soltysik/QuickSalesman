18b8e6ddea885bd030d50e6540cd4e47127dde34
Andrzej Soltysik <adzi11235@gmail.com>
2022-11-03 18:49:47 +0100

    Add ignoring optimization for benchmark

--- src/algorithms/benchmarks/Benchmark.h
+++ src/algorithms/benchmarks/Benchmark.h
@@ -6,15 +6,31 @@
 namespace qs::bench
 {
 
-template <typename DurationType, typename ResultT, typename... Args>
+template<typename DurationType, typename ResultT, typename... Args>
 auto run(algo::Algorithm<ResultT, Args...>&& algorithm,
-         const Args&... args) -> std::pair<typename algo::Algorithm<ResultT, Args...>::Result, utils::Clock::Time>
+         const Args& ... args) -> std::pair<typename algo::Algorithm<ResultT, Args...>::Result, utils::Clock::Time>
 {
-    auto clock = utils::Clock{};
+    auto clock = utils::Clock {};
     clock.start();
     auto result = algorithm.solve(args...);
     clock.stop();
     return {std::move(result), clock.getTime<DurationType>()};
+}
+
+template<typename DurationType, typename ResultT, typename... Args>
+auto runAverage(size_t times,
+                algo::Algorithm<ResultT, Args...>&& algorithm,
+                const Args& ... args) -> utils::Clock::Time
+{
+    auto clock = utils::Clock {};
+    clock.start();
+    for (auto i = size_t {}; i < times; i++)
+    {
+        auto result = algorithm.solve(args...);
+        utils::doNotOptimize(result);
+    }
+    clock.stop();
+    return clock.template getTime<DurationType>() / times;
 }
 
 }
--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -20,10 +20,28 @@ auto print(const std::ranges::range auto& range) -> void
     std::cout << '\n';
 }
 
-auto print(const auto& item, const auto&... items) -> void
+auto print(const auto& item, const auto& ... items) -> void
 {
     std::cout << item;
     ((std::cout << " " << items), ...) << "\n";
+}
+
+// Functions from GoogleBenchmark
+
+template<class Tp>
+auto doNotOptimize(Tp const& value) -> void
+{
+    asm volatile("" : : "r,m"(value) : "memory");
+}
+
+template<class Tp>
+auto doNotOptimize(Tp& value) -> void
+{
+#if defined(__clang__)
+    asm volatile("" : "+r,m"(value) : : "memory");
+#else
+    asm volatile("" : "+m,r"(value) : : "memory");
+#endif
 }
 
 }
